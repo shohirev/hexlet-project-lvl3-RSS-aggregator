6f785bb2bdc6a716ea6529b03b1859a4cb673b2f
shohirev <vishohirev@yandex.ru>
2021-03-10 14:46:06 +0500

    init i18next fixing

--- src/init.js
+++ src/init.js
@@ -43,9 +43,8 @@ export default () => {
   .then((t) => {
     view.init(t);
     view.renderTemplate.bind(view)();
-  });
 
-  const form = document.getElementById('rss-form');
+    const form = document.getElementById('rss-form');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -125,4 +124,86 @@ export default () => {
         watchedState.process = 'processingError';
       });
   });
+  });
+
+ /*const form = document.getElementById('rss-form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    watchedState.process = 'processingRequest';
+
+    const rssChannelUrl = document.getElementById('rss-input').value;
+
+    const rssUrlValidationSchema = yup
+      .string()
+      .notOneOf(watchedState.loadedFeeds, 'duplicationUrlError')
+      .matches(
+        /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([w#!:.?+=&%@!]))?/,
+        'invalidRssUrlError',
+      );
+
+    try {
+      rssUrlValidationSchema.validateSync(rssChannelUrl);
+    } catch (validationError) {
+      const errorMessage = validationError.errors[0];
+      watchedState.errors.push({ error: validationError, type: errorMessage });
+      watchedState.process = 'processingError';
+      return;
+    }
+
+    axios
+      .get(proxify(rssChannelUrl))
+      .then((response) => response.data)
+      .catch((networkError) => {
+        watchedState.errors.push({ error: networkError, type: 'networkError' });
+        watchedState.process = 'processingError';
+      })
+      .then((data) => {
+        const parsedRssChannel = parseRSS(data.contents);
+        watchedState.loadedFeeds.push(rssChannelUrl);
+
+        const feedId = watchedState.loadedFeeds.length;
+        const title = parsedRssChannel.querySelector('channel title')
+          .textContent;
+        const description = parsedRssChannel.querySelector(
+          'channel description',
+        ).textContent;
+        const link = parsedRssChannel.querySelector('channel link').textContent;
+        watchedState.feeds = [
+          {
+            feedId,
+            title,
+            description,
+            link,
+          },
+          ...watchedState.feeds,
+        ];
+
+        const currentFeedItems = Array.from(
+          parsedRssChannel.querySelectorAll('item'),
+          (itemNode) => {
+            const item = {
+              guid: itemNode.querySelector('guid').textContent,
+              title: itemNode.querySelector('title').textContent,
+              description: itemNode.querySelector('description').textContent,
+              link: itemNode.querySelector('link').textContent,
+            };
+            return item;
+          },
+        );
+
+        watchedState.items[feedId] = currentFeedItems;
+
+        watchedState.process = 'processingFeeds';
+
+        setTimeout(() => {
+          update(rssChannelUrl, 5000, watchedState, feedId);
+        }, 5000);
+      })
+      .catch((parsingError) => {
+        watchedState.errors.push({ error: parsingError, type: 'parsingError' });
+        watchedState.process = 'processingError';
+      });
+  });*/
 };
