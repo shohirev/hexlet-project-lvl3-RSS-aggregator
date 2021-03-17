a67e07d9b2967ac5e8f3dd4ef93dd9acade20c69
shohirev <vishohirev@yandex.ru>
2021-03-17 14:05:58 +0500

    simple init

--- src/init.js
+++ src/init.js
@@ -20,11 +20,21 @@ export default () => {
       view.renderTemplate.bind(view)();
 
       const form = document.getElementById('rss-form');
+      const input = document.getElementById('rss-input');
+      const button = document.querySelector('.btn[name=add]');
 
       form.addEventListener('submit', (e) => {
         e.preventDefault()
-        console.log('button click')
+        console.log('form submitted!')
         document.getElementById('feedback').textContent = 'RSS успешно загружен';
+      });
+
+      input.addEventListener('change', (e) => {
+        console.log('input changed!');
+      });
+
+      button.addEventListener('click', (e) => {
+        console.log('button clicked!');
       });
     });
     
